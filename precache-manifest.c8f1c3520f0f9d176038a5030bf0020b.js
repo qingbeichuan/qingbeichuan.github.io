9fd0d9d7357148a5768d90f8e089c9adc2c33e52
qingbeichuan <qingbeichuan@kuaidihelp.com>
2019-08-30 17:32:26 +0800

    kugou

---
+++ precache-manifest.c8f1c3520f0f9d176038a5030bf0020b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "821cfac1a4b18275485f",
+    "url": "/static/js/main.821cfac1.chunk.js"
+  },
+  {
+    "revision": "5152dcf46383d2b565fd",
+    "url": "/static/js/1.5152dcf4.chunk.js"
+  },
+  {
+    "revision": "821cfac1a4b18275485f",
+    "url": "/static/css/main.cfdfc505.chunk.css"
+  },
+  {
+    "revision": "5152dcf46383d2b565fd",
+    "url": "/static/css/1.da66f7ab.chunk.css"
+  },
+  {
+    "revision": "55c4f9b03871629fdd83181fdc3effda",
+    "url": "/index.html"
+  }
+];
